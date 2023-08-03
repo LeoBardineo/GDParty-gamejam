3817adf949ea745f5b65f44305ed0d9200c97bae
Rafahgol <rafalima245@hotmail.com>
2023-08-03 02:06:46 -0300

    Pulo e Pulo Duplo

--- GDParty GameJam/Assets/Scripts/Player.cs
+++ GDParty GameJam/Assets/Scripts/Player.cs
@@ -4,77 +4,50 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public float Speed, JumpForce;
-    private Rigidbody2D rig;
-    //Inicia double jump como falso
-    public bool DoubleJumpBool = false;
-    void Start()
+   public float speed;
+   public float jump;
+   public bool isJumping;
+   private float Move;
+   public Rigidbody2D rb;
+   //DoubleJump
+   public bool doubleJump = false;
+   void Start()
+   {
+
+   }
+
+   void Update()
+   {
+    Move = Input.GetAxis("Horizontal");
+    rb.velocity = new Vector2(speed * Move, rb.velocity.y);
+
+    if (Input.GetButtonDown("Jump") && isJumping == false)
     {
-        rig = GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2(rb.velocity.x, jump));
+        doubleJump = true;
+    }
+    if (Input.GetButtonDown("Jump") && isJumping == true && doubleJump == true)
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.AddForce(new Vector2(rb.velocity.x, jump));
+        doubleJump = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Jump();
-    }
+   }
 
-    void FixedUpdate()
-    {
-        Move();
-    }
-        void Move()
-    {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),0f,0f);
-        transform.position += movement * Time.deltaTime * Speed;
+   private void OnCollisionEnter2D(Collision2D other)
+   {
+        if (other.gameObject.CompareTag("ground"))
+        {
+            isJumping = false;
+        }
+   }
 
-        if(Input.GetAxis("Horizontal") > 0f)
+   private void OnCollisionExit2D(Collision2D other)
+   {
+        if(other.gameObject.CompareTag("ground"))
         {
-            //anim.SetBool("Walk",true);
-            transform.eulerAngles = new Vector3(0f,0f,0f);
+            isJumping = true;
         }
-        else if(Input.GetAxis("Horizontal") < 0f)
-        {
-            //anim.SetBool("Walk",true);
-            transform.eulerAngles = new Vector3(0f,180f,0f);
-        }
-        else
-        {
-            //anim.SetBool("Walk",false);
-        }
-    }
-
-        void Jump()
-    {
-/*         if(IsJumping && rig.velocity.y > 0)
-        {
-            anim.SetBool("Jump_UP",true);
-        }
-        else if(IsJumping && rig.velocity.y < 0)
-        {
-            anim.SetBool("Jump_UP",false);
-            anim.SetBool("Jump_DOWN",true);
-        } */
-        if(Input.GetButtonDown("Jump"))
-        {
-            rig.AddForce(new Vector2(0f,JumpForce),ForceMode2D.Impulse);
-            DoubleJumpBool = true;
-            //Som_Pulo.Play();
-        }
-        else if (Input.GetButtonDown("Jump") && DoubleJumpBool == true)
-        {
-            DoubleJump();
-            DoubleJumpBool = false;
-        }
-    }
-
-        void DoubleJump()
-    {
-            rig.velocity = new Vector2 (rig.velocity.x, 0);
-            rig.AddForce(new Vector2(0f,JumpForce),ForceMode2D.Impulse);
-            //anim.SetBool("DoubleJump",true);
-        
-    }
-
+   }
 }
