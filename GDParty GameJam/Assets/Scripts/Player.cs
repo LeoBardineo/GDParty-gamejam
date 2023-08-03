8bbf5610f91fa517cc22ca9f6525e1ad21255567
Guilherme Barbosa <guisbarbosa@ufrj.br>
2023-08-03 14:38:11 -0300

    mt coisa

--- GDParty GameJam/Assets/Scripts/Player.cs
+++ GDParty GameJam/Assets/Scripts/Player.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   public float speed;
-   public float jump;
-   public bool isJumping;
-   private float Move;
+   public float speed,jump;
+   public bool isJumping,Paused;
+//    private float Move;
    public Rigidbody2D rb;
+   private Animator anim;
+   private string currentState;
+   
    //DoubleJump
    public bool doubleJump = false;
+   
    void Start()
    {
-
+        anim = GetComponent<Animator>();
    }
 
-   void Update()
+   void FixedUpdate()
    {
-    Move = Input.GetAxis("Horizontal");
-    rb.velocity = new Vector2(speed * Move, rb.velocity.y);
+    Move();
 
     if (Input.GetButtonDown("Jump") && isJumping == false)
     {
@@ -28,11 +30,16 @@ public class Player : MonoBehaviour
     }
     if (Input.GetButtonDown("Jump") && isJumping == true && doubleJump == true)
     {
-        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.velocity = new Vector2(rb.velocity.x, 0f);
         rb.AddForce(new Vector2(rb.velocity.x, jump));
         doubleJump = false;
     }
+    // ChangeAnimationState("Player_Jump");
+   }
 
+   void Update()
+   {
+        Pausou();
    }
 
    private void OnCollisionEnter2D(Collision2D other)
@@ -40,6 +47,11 @@ public class Player : MonoBehaviour
         if (other.gameObject.CompareTag("ground"))
         {
             isJumping = false;
+        }
+
+        if (other.gameObject.CompareTag("obstacle"))
+        {
+            //morre
         }
    }
 
@@ -50,4 +62,43 @@ public class Player : MonoBehaviour
             isJumping = true;
         }
    }
+
+   void ChangeAnimationState( string newState)
+    {
+        if (currentState == newState) return;
+        anim.Play(newState);
+        currentState = newState;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector3(speed * Input.GetAxis("Horizontal"), rb.velocity.y,0f);
+        
+        if(Input.GetAxis("Horizontal") > 0f)
+        {
+            ChangeAnimationState("Player_Walk");
+            transform.eulerAngles = new Vector3(0f,0f,0f);
+        }
+        else if(Input.GetAxis("Horizontal") < 0f)
+        {
+            ChangeAnimationState("Player_Walk");
+            transform.eulerAngles = new Vector3(0f,180f,0f);
+        }
+        else
+        {
+            ChangeAnimationState("Player_Idle");
+        }
+    }
+    // --------------------------------------------------------
+    // Quando tiver as outras skills implementadas, colocar os states pras animações
+    //ChangeAnimationState("Player_Dash");
+    // ChangeAnimationState("Player_DoubleJump");
+
+    public void Pausou()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Paused = !Paused;
+        }
+    }
 }
