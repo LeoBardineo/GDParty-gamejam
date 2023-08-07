ef88fc8778df6d588881c6d069ffbce082fdb1fe
Leonardo de Melo <45073941+LeoBardineo@users.noreply.github.com>
2023-08-06 22:31:56 -0300

    cena final criada

---
+++ GDParty GameJam/Assets/Porta.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Porta : MonoBehaviour
+{
+    bool podeInteragir = false;
+    public Animator anim;
+    public GameObject cenaFinal;
+
+    void Update()
+    {
+        if ((podeInteragir && Player.almasColetadas >= 6) || Input.GetKeyDown(KeyCode.Tab))
+        {
+            StartCoroutine(TrocarCena());
+        }
+    }
+
+    IEnumerator TrocarCena()
+    {
+        cenaFinal.SetActive(true);
+        anim.Play("CenaFinal");
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("Final");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            podeInteragir = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            podeInteragir = false;
+        }
+    }
+}
